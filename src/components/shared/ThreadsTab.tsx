8bce6bc3c1c4f2f4f676329a1f81aec4595abfbf
Dev Rudra <rudradev252@gmail.com>
2024-01-11 00:14:20 +0530

    Added infinite loading to user profile page

--- src/components/shared/ThreadsTab.tsx
+++ src/components/shared/ThreadsTab.tsx
@@ -1,38 +1,99 @@
+"use client";
 import { fetchUserPosts } from "@/lib/actions/threads.actions";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import dynamic from "next/dynamic";
 const ThreadCard = dynamic(() => import("@/components/cards/ThreadCard"));
-import { Divider } from "@nextui-org/react";
+import { Divider, Spinner } from "@nextui-org/react";
+import { useInView } from "react-intersection-observer";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ThreadCardSkeleton from "../Skeleton/ThreadCardSkeleton";
 
 interface ThreadsTabProps {
   currentUserId: string;
   id: string;
 }
-const ThreadsTab: FC<ThreadsTabProps> = async ({ id, currentUserId }) => {
-  const userThreads = await fetchUserPosts({ id: id });
+
+async function fetchData({ pageParam, id }: { pageParam: number; id: string }) {
+  console.log("The is is", id);
+
+  return await fetchUserPosts({ id: id });
+}
+
+const ThreadsTab: FC<ThreadsTabProps> = ({ id, currentUserId }) => {
+  // const userThreads = await fetchUserPosts({ id: id });
+
+  const { ref, inView } = useInView({ threshold: 1 });
+
+  const {
+    data: userThreads,
+    isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["threads"],
+    queryFn: (e) => fetchData({ id: id, pageParam: e.pageParam }),
+    initialPageParam: 1,
+    getNextPageParam: (prev, next) => {
+      if (prev && next) {
+        if (prev?.isNext) {
+          return next.length + 1;
+        } else {
+          return undefined;
+        }
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (inView) {
+      console.log("yes");
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
+
+  const Threads = userThreads?.pages.map((e) => e);
+
+  if (!Threads) {
+    return null;
+  }
+
+  if (isLoading) {
+    return <ThreadCardSkeleton noOfCards={5} />;
+  }
 
   return (
     <section className="mt-9 flex flex-col gap-10">
-      {userThreads.map((thread) => {
-        return (
-          <>
-            <Divider />
-            <ThreadCard
-              key={thread.id}
-              image={thread.image}
-              imageDesc={thread.imageDesc}
-              id={thread.id}
-              author={thread.author}
-              content={thread.text}
-              createdAt={thread.createdAt}
-              // currentUser={currentUserId}
-              parentId={thread.parentId}
-              username={thread.author.username as string}
-              isDedicatedPage={false}
-            />
-          </>
-        );
-      })}
+      {!isLoading && Threads ? (
+        Threads.map((thread) => {
+          if (!thread) return null;
+          return thread.userThread.map((showThread, i) => {
+            return (
+              <div
+                ref={i + 1 === thread.userThread.length ? ref : undefined}
+                key={showThread.id}
+              >
+                <ThreadCard
+                  image={showThread.image}
+                  imageDesc={showThread.imageDesc}
+                  id={showThread.id}
+                  author={showThread.author}
+                  content={showThread.text}
+                  createdAt={showThread.createdAt}
+                  // currentUser={currentUserId}
+                  parentId={showThread.parentId}
+                  username={showThread.author.username as string}
+                  isDedicatedPage={false}
+                />
+                <Divider />
+              </div>
+            );
+          });
+        })
+      ) : (
+        <div className="w-full mt-5 flex items-center justify-center">
+          {isFetchingNextPage && <Spinner className="mx-auto" />}
+        </div>
+      )}
     </section>
   );
 };
